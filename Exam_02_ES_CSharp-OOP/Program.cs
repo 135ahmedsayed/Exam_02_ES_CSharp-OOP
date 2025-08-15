6080caee74f8c874ce74441407264c77995c486f
135ahmedsayed <ahmedshkra246@gmail.com>
2025-08-15 19:52:38 +0300

    Add project files.

---
+++ Exam_02_ES_CSharp-OOP/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Exam_02_ES_CSharp_OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.ReadLine();
+        }
+    }
+}
