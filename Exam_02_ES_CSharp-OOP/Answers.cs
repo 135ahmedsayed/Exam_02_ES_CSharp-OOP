964e710bcb8dff33ff2a1a72f0d2f5b7cc70c513
135ahmedsayed <ahmedshkra246@gmail.com>
2025-08-15 20:00:35 +0300

    Created Classes

---
+++ Exam_02_ES_CSharp-OOP/Answers.cs
@@ -0,0 +1,22 @@
+﻿namespace Exam_02_ES_CSharp_OOP;
+
+internal class Answers
+{
+    public int AnswerID { get; set; }
+    public string AnswerText { get; set; } = default!;
+    public Answers()
+    {
+
+    }
+    // Answers for TF
+    public Answers(int Id, string Text)
+    {
+        AnswerID = Id;
+        AnswerText = Text;
+    }
+    public override string ToString()
+    {
+        return $"{AnswerID} - {AnswerText}";
+    }
+
+}
