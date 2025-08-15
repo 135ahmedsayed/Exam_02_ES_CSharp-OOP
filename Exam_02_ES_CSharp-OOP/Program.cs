9a35d1f99df319419007bbf71c81cda4f666a985
135ahmedsayed <ahmedshkra246@gmail.com>
2025-08-15 20:05:30 +0300

    Validation in Main Funtion

--- Exam_02_ES_CSharp-OOP/Program.cs
+++ Exam_02_ES_CSharp-OOP/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            
+            //// Create Exam
+            Subject subject = new Subject();
+            subject.CreateExam();
+
+            // I ask you to start the exam or not
+            char start;
+            do
+            {
+                Console.WriteLine("Do you want To start Exam (y | n)");
+                Char.TryParse(Console.ReadLine()!, out start);
+            }
+            while (start is not ('y' or 'Y' or 'n' or 'N'));
+            Console.Clear();
+            if (start is 'y' or 'Y')
+                subject.SubjectExam.ShowExam();
+            Console.WriteLine("Thank You");
             Console.ReadLine();
         }
     }
